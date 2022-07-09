2c6402fbb53ac52eaf299ad10f0c4e8722635077
Hítalo Bruno <hitalo.bruno@salvus.me>
2022-07-09 18:17:05 -0300

    fix in question 'f'

--- 1-lista-comando-condicionais/f-na-fogueira.py
+++ 1-lista-comando-condicionais/f-na-fogueira.py
@@ -103,85 +103,99 @@ pontuacao_2 = int(input())
 nome_3 = input()
 pontuacao_3 = int(input())
 
+# primeiro lugar
 nome_primeiro_lugar = nome_1
 pontuacao_primeiro_lugar = pontuacao_1
+# segundo lugar
 nome_segundo_lugar = nome_1
 pontuacao_segundo_lugar = pontuacao_1
+# terceiro lugar
 nome_terceiro_lugar = nome_1
 pontuacao_terceiro_lugar = pontuacao_1
 
-# definir primeiro e segundo lugar
+# comparação do valor 2 com valor 1
 if pontuacao_2 < pontuacao_primeiro_lugar:
-    nome_segundo_lugar = nome_primeiro_lugar
+    # segundo lugar parcial
     pontuacao_segundo_lugar = pontuacao_primeiro_lugar
-    nome_primeiro_lugar = nome_2
+    nome_segundo_lugar = nome_primeiro_lugar
+    # primeiro lugar parcial
     pontuacao_primeiro_lugar = pontuacao_2
-elif pontuacao_2 == pontuacao_primeiro_lugar:
-    if len(nome_2) < len(nome_primeiro_lugar):
-        nome_segundo_lugar = nome_primeiro_lugar
-        pontuacao_segundo_lugar = pontuacao_primeiro_lugar
-        nome_primeiro_lugar = nome_2
-        pontuacao_primeiro_lugar = pontuacao_2
-    else:
-        nome_segundo_lugar = nome_2
-        pontuacao_segundo_lugar = pontuacao_2
+    nome_primeiro_lugar = nome_2
 else:
-    nome_segundo_lugar = nome_2
-    pontuacao_segundo_lugar = pontuacao_2
+    if pontuacao_2 == pontuacao_primeiro_lugar:
+        if nome_2 < nome_primeiro_lugar:
+            # segundo lugar parcial
+            pontuacao_segundo_lugar = pontuacao_primeiro_lugar
+            nome_segundo_lugar = nome_primeiro_lugar
+            # primeiro lugar parcial
+            pontuacao_primeiro_lugar = pontuacao_2
+            nome_primeiro_lugar = nome_2
+        else:
+            # segundo lugar parcial
+            pontuacao_segundo_lugar = pontuacao_2
+            nome_segundo_lugar = nome_2
+    else:
+        if pontuacao_2 > pontuacao_primeiro_lugar:
+            # segundo lugar parcial
+            pontuacao_segundo_lugar = pontuacao_2
+            nome_segundo_lugar = nome_2
 
-# definir terceiro lugar
+# comparação do valor 3 com os demais
 if pontuacao_3 < pontuacao_primeiro_lugar:
-    nome_terceiro_lugar = nome_segundo_lugar
-    nome_segundo_lugar = nome_primeiro_lugar
-    nome_primeiro_lugar = nome_3
+    # terceiro lugar parcial
     pontuacao_terceiro_lugar = pontuacao_segundo_lugar
+    nome_terceiro_lugar = nome_segundo_lugar
+    # segundo lugar parcial
     pontuacao_segundo_lugar = pontuacao_primeiro_lugar
+    nome_segundo_lugar = nome_primeiro_lugar
+    # primeiro lugar parcial
     pontuacao_primeiro_lugar = pontuacao_3
-elif pontuacao_3 == pontuacao_primeiro_lugar:
-    if len(nome_3) < len(nome_primeiro_lugar):
-        nome_terceiro_lugar = nome_segundo_lugar
-        nome_segundo_lugar = nome_primeiro_lugar
-        nome_primeiro_lugar = nome_3
-        pontuacao_terceiro_lugar = pontuacao_segundo_lugar
-        pontuacao_segundo_lugar = pontuacao_primeiro_lugar
-        pontuacao_primeiro_lugar = pontuacao_3
+    nome_primeiro_lugar = nome_3
+else:
+    if pontuacao_3 == pontuacao_primeiro_lugar:
+        if nome_3 < nome_primeiro_lugar:
+            # terceiro lugar parcial
+            pontuacao_terceiro_lugar = pontuacao_segundo_lugar
+            nome_terceiro_lugar = nome_segundo_lugar
+            # segundo lugar parcial
+            pontuacao_segundo_lugar = pontuacao_primeiro_lugar
+            nome_segundo_lugar = nome_primeiro_lugar
+            # primeiro lugar parcial
+            pontuacao_primeiro_lugar = pontuacao_3
+            nome_primeiro_lugar = nome_3
+        else:
+            # terceiro lugar
+            pontuacao_terceiro_lugar = pontuacao_segundo_lugar
+            nome_terceiro_lugar = nome_segundo_lugar
+            # segundo lugar
+            pontuacao_segundo_lugar = pontuacao_3
+            nome_segundo_lugar = nome_3
     else:
         if pontuacao_3 < pontuacao_segundo_lugar:
-            nome_terceiro_lugar = nome_segundo_lugar
-            nome_segundo_lugar = nome_3
+            # terceiro lugar
             pontuacao_terceiro_lugar = pontuacao_segundo_lugar
+            nome_terceiro_lugar = nome_segundo_lugar
+            # segundo lugar
             pontuacao_segundo_lugar = pontuacao_3
-        elif pontuacao_3 == pontuacao_segundo_lugar:
-            if len(nome_3) < len(nome_segundo_lugar):
-                nome_terceiro_lugar = nome_segundo_lugar
-                nome_segundo_lugar = nome_3
-                pontuacao_terceiro_lugar = pontuacao_segundo_lugar
-                pontuacao_segundo_lugar = pontuacao_3
-            else:
-                nome_terceiro_lugar = nome_3
-                pontuacao_terceiro_lugar = pontuacao_3
+            nome_segundo_lugar = nome_3
         else:
-            nome_terceiro_lugar = nome_3
-            pontuacao_terceiro_lugar = pontuacao_3
-elif pontuacao_primeiro_lugar < pontuacao_3 < pontuacao_segundo_lugar:
-    print('entrei')
-    nome_terceiro_lugar = nome_segundo_lugar
-    nome_segundo_lugar = nome_3
-    pontuacao_terceiro_lugar = pontuacao_segundo_lugar
-    pontuacao_segundo_lugar = pontuacao_3
-elif pontuacao_3 > pontuacao_primeiro_lugar and pontuacao_3 == pontuacao_segundo_lugar:
-    print('aqui')
-    if len(nome_3) < len(nome_segundo_lugar):
-        nome_terceiro_lugar = nome_segundo_lugar
-        nome_segundo_lugar = nome_3
-        pontuacao_terceiro_lugar = pontuacao_segundo_lugar
-        pontuacao_segundo_lugar = pontuacao_3
-    else:
-        nome_terceiro_lugar = nome_3
-        pontuacao_terceiro_lugar = pontuacao_3
-else:
-    nome_terceiro_lugar = nome_3
-    pontuacao_terceiro_lugar = pontuacao_3
+            if pontuacao_3 == pontuacao_segundo_lugar:
+                if nome_3 < nome_segundo_lugar:
+                    # terceiro lugar
+                    pontuacao_terceiro_lugar = pontuacao_segundo_lugar
+                    nome_terceiro_lugar = nome_segundo_lugar
+                    # segundo lugar
+                    pontuacao_segundo_lugar = pontuacao_3
+                    nome_segundo_lugar = nome_3
+                else:
+                    # terceiro lugar
+                    pontuacao_terceiro_lugar = pontuacao_3
+                    nome_terceiro_lugar = nome_3
+            else:
+                if pontuacao_3 > pontuacao_segundo_lugar:
+                    # terceiro lugar
+                    pontuacao_terceiro_lugar = pontuacao_3
+                    nome_terceiro_lugar = nome_3
 
 print('{0} {1}'.format(nome_primeiro_lugar, pontuacao_primeiro_lugar))
 print('{0} {1}'.format(nome_segundo_lugar, pontuacao_segundo_lugar))
